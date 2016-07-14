805ce2b19c0c8d12c3b5f3728a3809d24a8529ce
Francis Saul <francis@mummybot.com>
2016-07-14 13:49:26 +0200

    Added TPBC recent posts widget

---
+++ class-wp-widget-recent-posts.php
@@ -0,0 +1,171 @@
+<?php
+/**
+ * Widget API: WP_Widget_Recent_Posts class
+ * This is a fork of the Wordpress recent posts widget to modify the widget template
+ * to display the excerpt, modify the HTML and add a custom read more button at the bottom of the widget.
+ *
+ * @package WordPress
+ * @subpackage Widgets
+ * @since 4.4.0
+ */
+
+/**
+ * Core class used to implement a Recent Posts widget.
+ *
+ * @since 2.8.0
+ *
+ * @see WP_Widget
+ */
+class TPBC_WP_Widget_Recent_Posts extends WP_Widget {
+
+	/**
+	 * Sets up a new Recent Posts widget instance.
+	 *
+	 * @since 2.8.0
+	 * @access public
+	 */
+	public function __construct() {
+		$widget_ops = array(
+			'classname' => 'widget_recent_entries_tpbc',
+			'description' => __( 'Your site&#8217;s most recent Posts.' ),
+			'customize_selective_refresh' => true,
+		);
+		parent::__construct( 'recent-posts-tpbc', __( 'Recent Posts TPBC' ), $widget_ops );
+		$this->alt_option_name = 'widget_recent_entries_tpbc';
+	}
+
+	/**
+	 * Outputs the content for the current Recent Posts widget instance.
+	 *
+	 * @since 2.8.0
+	 * @access public
+	 *
+	 * @param array $args     Display arguments including 'before_title', 'after_title',
+	 *                        'before_widget', and 'after_widget'.
+	 * @param array $instance Settings for the current Recent Posts widget instance.
+	 */
+	public function widget( $args, $instance ) {
+		if ( ! isset( $args['widget_id'] ) ) {
+			$args['widget_id'] = $this->id;
+		}
+
+		$title = ( ! empty( $instance['title'] ) ) ? $instance['title'] : __( 'Recent Posts' );
+
+		/** This filter is documented in wp-includes/widgets/class-wp-widget-pages.php */
+		$title = apply_filters( 'widget_title', $title, $instance, $this->id_base );
+
+		$number = ( ! empty( $instance['number'] ) ) ? absint( $instance['number'] ) : 5;
+		if ( ! $number )
+			$number = 5;
+		$show_date = isset( $instance['show_date'] ) ? $instance['show_date'] : false;
+
+		$link = ( ! empty( $instance['link'] ) ) ? $instance['link'] : false;
+
+		/**
+		 * Filter the arguments for the Recent Posts widget.
+		 *
+		 * @since 3.4.0
+		 *
+		 * @see WP_Query::get_posts()
+		 *
+		 * @param array $args An array of arguments used to retrieve the recent posts.
+		 */
+		$r = new WP_Query( apply_filters( 'widget_posts_args', array(
+			'posts_per_page'      => $number,
+			'no_found_rows'       => true,
+			'post_status'         => 'publish',
+			'ignore_sticky_posts' => true
+		) ) );
+
+		if ($r->have_posts()) :
+		?>
+		<?php echo $args['before_widget']; ?>
+		<div class="widgetNews">
+		<?php if ( $title ) {
+			echo $args['before_title'] . $title . $args['after_title'];
+		} ?>
+		<ul>
+		<?php while ( $r->have_posts() ) : $r->the_post(); ?>
+			<li>
+				<h3>
+					<a href="<?php the_permalink(); ?>"><?php get_the_title() ? the_title() : the_ID(); ?></a>
+					<?php if ( $show_date ) : ?>
+						<span class="post-date"><?php echo get_the_date('j F Y'); ?></span>
+					<?php endif; ?>
+				</h3>
+				<?php print the_excerpt(); ?>
+			</li>
+		<?php endwhile; ?>
+		</ul>
+
+		<?php if ( $link ) {
+			echo '<a href="' . $link . '" class="btn">See all news</a>';
+		} ?>
+		</div>
+		<?php echo $args['after_widget']; ?>
+		<?php
+		// Reset the global $the_post as this query will have stomped on it
+		wp_reset_postdata();
+
+		endif;
+	}
+
+	/**
+	 * Handles updating the settings for the current Recent Posts widget instance.
+	 *
+	 * @since 2.8.0
+	 * @access public
+	 *
+	 * @param array $new_instance New settings for this instance as input by the user via
+	 *                            WP_Widget::form().
+	 * @param array $old_instance Old settings for this instance.
+	 * @return array Updated settings to save.
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance['title'] = sanitize_text_field( $new_instance['title'] );
+		$instance['number'] = (int) $new_instance['number'];
+		$instance['show_date'] = isset( $new_instance['show_date'] ) ? (bool) $new_instance['show_date'] : false;
+		$instance['link'] = sanitize_text_field( $new_instance['link'] );
+		return $instance;
+	}
+
+	/**
+	 * Outputs the settings form for the Recent Posts widget.
+	 *
+	 * @since 2.8.0
+	 * @access public
+	 *
+	 * @param array $instance Current settings.
+	 */
+	public function form( $instance ) {
+		$title     = isset( $instance['title'] ) ? esc_attr( $instance['title'] ) : '';
+		$number    = isset( $instance['number'] ) ? absint( $instance['number'] ) : 5;
+		$show_date = isset( $instance['show_date'] ) ? (bool) $instance['show_date'] : false;
+		$link     = isset( $instance['link'] ) ? esc_attr( $instance['link'] ) : '';
+?>
+		<p><label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $title; ?>" /></p>
+
+		<p><label for="<?php echo $this->get_field_id( 'number' ); ?>"><?php _e( 'Number of posts to show:' ); ?></label>
+		<input class="tiny-text" id="<?php echo $this->get_field_id( 'number' ); ?>" name="<?php echo $this->get_field_name( 'number' ); ?>" type="number" step="1" min="1" value="<?php echo $number; ?>" size="3" /></p>
+
+		<p><input class="checkbox" type="checkbox"<?php checked( $show_date ); ?> id="<?php echo $this->get_field_id( 'show_date' ); ?>" name="<?php echo $this->get_field_name( 'show_date' ); ?>" />
+		<label for="<?php echo $this->get_field_id( 'show_date' ); ?>"><?php _e( 'Display post date?' ); ?></label></p>
+
+		<p><label for="<?php echo $this->get_field_id( 'link' ); ?>"><?php _e( 'Link:' ); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id( 'link' ); ?>" name="<?php echo $this->get_field_name( 'link' ); ?>" type="text" value="<?php echo $link; ?>" /></p>
+<?php
+	}
+}
+
+function custom_excerpt_length( $length ) {
+	return 20;
+}
+add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
+function sbt_custom_excerpt_more( $output ) {
+	return preg_replace('/<a[^>]+>Continue reading.*?<\/a>/i','',$output);
+}
+add_filter( 'the_excerpt', 'sbt_custom_excerpt_more', 20 );
+
+add_action( 'widgets_init', create_function('', 'return register_widget("TPBC_WP_Widget_Recent_Posts");') );
---
+++ tpbc-plugin.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Site Plugin for tpbc
+Description: Site specific code changes for tpbc
+*/
+/* Start Adding Functions Below this Line */
+include(dirname(__FILE__).'/class-wp-widget-recent-posts.php');
+// include(dirname(__FILE__).'/featured-post.php');
+
+/* Stop Adding Functions Below this Line */
+?>
